c5ac95b0d9069ca12e5ea7a997b968aaf2d5b109
Dat Nguyen <dat.nguyen@ge.com>
2016-12-05 20:28:51 -0800

    fixing procfile

--- services.js
+++ services.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var pg = require('pg');
-var conString = "postgres://zuktmeqgijrqvk:IJTJFbxJpg8d7fyJ5N2P1s6e0g@ec2-50-19-227-171.compute-1.amazonaws.com:5432/dbe6e2j6v1ah3l";
-
-var client = new pg.Client(conString);
-client.connect();
+// var pg = require('pg');
+// var conString = "postgres://zuktmeqgijrqvk:IJTJFbxJpg8d7fyJ5N2P1s6e0g@ec2-50-19-227-171.compute-1.amazonaws.com:5432/dbe6e2j6v1ah3l";
+//
+// var client = new pg.Client(conString);
+// client.connect();
 
 
 var app = express();
