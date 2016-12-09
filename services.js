1469ab6c53923f1848e031dc8ea1059336eb7052
Dat Nguyen <dat.nguyen@ge.com>
2016-12-08 23:21:10 -0800

    query test

--- services.js
+++ services.js
@@ -5,8 +5,8 @@ var conString = process.env.DATABASE_URL;
 var client = new pg.Client(conString);
 client.connect();
 
-// const query = client.query();
-
+// const query = client.query(
+//     'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
 // query.on('end', () => { client.end(); });
 
 var app = express();
@@ -20,6 +20,35 @@ var router = express.Router();
 router.get('/info', function(req, res) {
     res.json({ database: process.env.DATABASE_URL });
 });
+
+
+router.get('/competitors', function(req, res) {
+
+    pg.connect(connectionString, (err, client, done) =>
+    {
+        // Handle connection errors
+        if(err)
+        {
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+        // SQL Query > Select Data
+        const query = client.query('SELECT * FROM competitors');
+        // Stream results back one row at a time
+        query.on('row', (row) => {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', () => {
+            done();
+            console.log(results)
+            return res.json(results);
+        });
+    });
+
+});
+
 
 router.get('/', function(req, res) {
     res.json({ message: 'BJJ-DB' });   
